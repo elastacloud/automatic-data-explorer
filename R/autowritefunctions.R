5febb6d6750d9709cb36508052be2d00e32b4430
AndBooth <andrew@elastacloud.com>
2017-07-28 17:17:41 +0100

    documentation for insertChunk function

--- R/autowritefunctions.R
+++ R/autowritefunctions.R
@@ -1,8 +1,19 @@
 
-
+#' Insert a code chunk into an R Markdown file
+#' @description This function can be used to insert any line of code
+#' into an R Markdown file. The chunk will be inserted into the file called
+#' \code{filename}, which must exist in the current working directory. Chaining
+#' with \code{\%>\%} is possible, by returning \code{filename} from the function.
+#' @param filename Character giving the name of the R Markdown file that the
+#' chunk should be inserted into, minus the \code{.Rmd} extension
+#' @param object Can be any unquoted line of R code, i.e. a call to a function
+#' @param title Character giving an optional markdown header that will can
+#' included before the code chunk e.g. of the form \code{# Header, ## Header,
+#' ### Header}
+#' @return Returns \code{filename} so that functions can be chained to write
+#' to the same file consecutively
 insertChunk <- function(filename, object,
-                       title = NULL,
-                       con = NULL) {
+                       title = NULL) {
 
   do <- deparse(substitute(object))
 
@@ -15,6 +26,39 @@ insertChunk <- function(filename, object,
   filename
 
 }
+
+
+insertYAML <- function(filename,
+                       title = NULL, author = NULL, date = NULL,
+                       output = "html_document") {
+
+  if(is.null(date)) {
+    date <- Sys.Date()
+  }
+
+  title <- paste0("title: ", title)
+  author <- paste0("author: ", author)
+  output <- paste0("output: ", output)
+  date <- paste0("date: ", "'", date, "'")
+
+  header <- c("---", title, author, date, output, "---")
+
+  write(header, file = filename, append = TRUE)
+
+  filename
+}
+
+insertLibraries <- function(filename,
+                            libraries = c("AutoExploreR", "ggplot2",
+                                          "plotly")) {
+
+  libraries <- purrr::map_chr(libraries, ~ paste0("library(", ., ")"))
+
+  write(c("```{r}", libraries, "```"), file = filename, append = TRUE)
+
+  filename
+}
+
 
 
 
