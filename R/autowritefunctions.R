df75464d6363b0366cc64c2fa0e69959a8b434d0
AndBooth <andrew@elastacloud.com>
2017-07-31 11:40:39 +0100

    insertChunk can now insert multiple lines + documentation

--- R/autowritefunctions.R
+++ R/autowritefunctions.R
@@ -1,29 +1,58 @@
 
 #' Insert a code chunk into an R Markdown file
-#' @description This function can be used to insert any line of code
+#' @description This function can be used to insert any line or lines of code
 #' into an R Markdown file. The chunk will be inserted into the file called
-#' \code{filename}, which must exist in the current working directory. Chaining
+#' \code{filename.Rmd}, which must exist in the current working directory. Chaining
 #' with \code{\%>\%} is possible, by returning \code{filename} from the function.
 #' @param filename Character giving the name of the R Markdown file that the
 #' chunk should be inserted into, minus the \code{.Rmd} extension
-#' @param object Can be any unquoted line of R code, i.e. a call to a function
-#' @param title Character giving an optional markdown header that will can
+#' @param object Can be any unquoted line of R code, i.e. a call to a function to insert a
+#' single line. To insert more than one line, pass as a vector of type \code{"expression"}.
+#' @param title Single or vector of character/s giving an optional markdown header that will be
 #' included before the code chunk e.g. of the form \code{# Header, ## Header,
-#' ### Header}
+#' ### Header}. Pass as a vector of titles of equal length to object expressions
+#' if multiple headers required.
 #' @return Returns \code{filename} so that functions can be chained to write
 #' to the same file consecutively
+#' @examples ## Insert single chunk, chained from createFile() function
+#' createFile("Mtcars") %>% insertLibraries() %>%
+#'            insertChunk(summary(mtcars), title = "#Summary")
+#'
+#' ## Insert multiple chunks to existing file, with headers
+#'
+#' insertChunk("Mtcars.Rmd",
+#'             expression(
+#'             plot(mtcars$cyl, mtcars$hp),
+#'             multivariateCorrelation(mtcars)),
+#'             title= c("##Scatter Plot",
+#'                      "##Correlations"))
 insertChunk <- function(filename, object,
                        title = NULL) {
 
-  do <- deparse(substitute(object))
+  if(is.expression(object)) {
+    deparsed <- purrr::map_chr(object, ~ deparse(.))
 
-  if(is.null(title)) {
-    write(c("```{r}", do, "```"), file = filename, append = TRUE)
+    for(i in seq_along(deparsed)) {
+      writeChunk(deparsed[[i]], filename = filename, title = title[[i]])
+    }
   } else {
-    write(c(title,"```{r}", do, "```"), file = filename, append = TRUE)
+    do <- deparse(substitute(object))
+    writeChunk(do, filename = filename, title = title)
   }
 
   filename
+
+}
+
+
+writeChunk <- function(object, filename,
+                       title = NULL) {
+
+  if(is.null(title)) {
+    write(c("```{r}", object, "```"), file = filename, append = TRUE)
+  } else {
+    write(c(title,"```{r}", object, "```"), file = filename, append = TRUE)
+  }
 
 }
 
