10ad33097362053734c2a49dc0941e67947a8f96
AndBooth <andrew@elastacloud.com>
2017-08-01 10:55:27 +0100

    Added unit tests for correlation plot function

--- tests/testthat/testvisualisationfuncs.R
+++ tests/testthat/testvisualisationfuncs.R
@@ -11,10 +11,33 @@ test_that("Histogram visualisation function",{
                          B = runif(50,10,20),
                          C = seq(1,50,1))
 
-  plot <- gghistogram(testdata, "A")
-  plot_2 <- gghistogram(testdata, "C", interactiveplot = T)
+  plot <- autoHistogramPlot(testdata, "A")
+  plot_2 <- autoHistogramPlot(testdata, "C", interactiveplot = T)
 
   expect_true(all(class(plot) == c("gg", "ggplot")))
   expect_true(all(class(plot_2) == c("plotly", "htmlwidget")))
+
+
+})
+
+
+test_that("Correlation plot function", {
+
+  m <- multivariateCorrelation(mtcars, output = "matrix")
+
+  plot <- autoCorrelationPlot(m)
+  plot_2 <- autoCorrelationPlot(m, interactiveplot = T)
+
+  expect_true(class(plot) == "matrix")
+  expect_true(all(class(plot_2) == c("plotly", "htmlwidget")))
+
+  m_A <- m
+  m_A[2, 1] <- 0.654
+
+  m_B <- m
+  m_B[2, 1] <- -1.12
+
+  expect_error(autoCorrelationPlot(m_A))
+  expect_error(autoCorrelationPlot(m_B, interactiveplot = T))
 
 })
