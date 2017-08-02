00fbcffba2b2f436e8b3561901ec2933c1e18a9c
AndBooth <andrew@elastacloud.com>
2017-08-02 14:23:43 +0100

    Updated insertChunk documentation

--- R/autowritefunctions.R
+++ R/autowritefunctions.R
@@ -9,12 +9,15 @@
 #' @param object Can be any unquoted line of R code, i.e. a call to a function to insert a
 #' single line. To insert more than one line, pass as a vector of type \code{"expression"}.
 #' @param title Single or vector of character/s giving an optional markdown header that will be
-#' included before the code chunk e.g. of the form \code{# Header, ## Header,
-#' ### Header}. Pass as a vector of titles of equal length to object expressions
+#' included before the code chunk e.g. of the form \code{"# Header", "## Header",
+#' "### Header"}. Pass as a vector of titles of equal length to object expressions
 #' if multiple headers required.
 #' @return Returns \code{filename} so that functions can be chained to write
 #' to the same file consecutively
 #' @examples ## Insert single chunk, chained from createFile() function
+#'
+#' \dontrun{
+#'
 #' createFile("Mtcars") %>% insertLibraries() %>%
 #'            insertChunk(summary(mtcars), title = "#Summary")
 #'
@@ -26,6 +29,7 @@
 #'             multivariateCorrelation(mtcars)),
 #'             title= c("##Scatter Plot",
 #'                      "##Correlations"))
+#'}
 insertChunk <- function(filename, object,
                        chunktitle = NULL) {
 
